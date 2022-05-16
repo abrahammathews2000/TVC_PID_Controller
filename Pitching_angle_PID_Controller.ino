7c172bc4ddf7eb03bda79ef056fcc027c6f4fa5e
Abraham Mathews <50496762+abrahammathews2000@users.noreply.github.com>
2022-05-16 20:35:26 +0530

    Update Pitching_angle_PID_Controller.ino

--- Pitching_angle_PID_Controller.ino
+++ Pitching_angle_PID_Controller.ino
@@ -1,14 +1,14 @@
-
 /*
 FileName: Pitching_angle_PID_Controller.ino
 Last updated: 16 May 2022
 Author: Adithya Praveen
-Last updated by: Abraham Mathews (13:04 pm, 16 May 2022)
+Last updated by: Abraham Mathews (8:26pm pm, 16 May 2022)
 Description: This program is used to store the pitch angle in the SD Card and produce PID output corresponding the pitch angle and the reference pitch angle. File name in which angle is saved is: pitchAng.txt. Y angle from MPU6050 is the pitch angle
 
 Revision Notes:
 1. Placed calling of pid function outside the if loop
 of writing of angle to SD Card
+2. Tried to arrange the code according to Barr C Standards
 */
 
 #include <MPU6050_light.h>// load mpu6050 library
@@ -19,83 +19,104 @@ of writing of angle to SD Card
 #include <SD.h> //Load SD card library
 #include<SPI.h> //Load SPI Library
 
-MPU6050 mpu(Wire);
-double pitch;//for saving pitch angle
-double xangle, yangle, zangle;// for storing x, y,z angles
-int chipSelect = 10; //chipSelect pin for the SD card Reader
-File angleFile; //Data object you will write your sesnor data to
-unsigned long myTime; // To store sample time
-double Output_max=20;//30 // servo maximum rotation from 90
-double Output_min=-20;//30 //servo minimum rotation from 90
-double Kp=0.5;//0.2 //proportional gain
-double Ki=0.01;//0.03 //integral gain
-double Kd=0.3;//0.1 // derivative gain
-double setpoint=0; //reference angle
-double pidXout; // pitch output from PID
-AutoPID pitchPID(&pitch,&setpoint,&pidXout,Output_min,Output_max, Kp, Ki, Kd);// autopid_pitch
-Servo Xservo;// pitch servo
-int servoAngleDefault = 85 ; //Default servoAngle i.e. 0th posn of TVC
-int servopin_X=3;//servo X pin
+//Reference Angle for the Rocket
+	double setpoint=0;
+
+//Variables related with MPU6050
+	double pitch;//for saving pitch angle
+	double xangle, yangle, zangle;// for storing x, y,z angles
+	unsigned long myTime; // To store sample time
+	
+	// Defining MPU6050 Function as mpu
+		MPU6050 mpu(Wire);
+
+
+//Variables and Objects related with SD Card
+	int chipSelect = 10; //chipSelect pin for the SD card Reader
+	File angleFile; //Data object you will write your sesnor data to
+	
+//Variables related with Servo
+	Servo Xservo;// pitch servo
+	int servoAngleDefault = 85 ; //Default servoAngle i.e. 0th posn of TVC
+	int servopin_X=3;//servo X pin
+
+
+
+// Variables and etc. related with PID controller
+	double Output_max=20;//30 // servo maximum rotation from 90
+	double Output_min=-20;//30 //servo minimum rotation from 90
+	double Kp=0.5;//0.2 //proportional gain
+	double Ki=0.01;//0.03 //integral gain
+	double Kd=0.3;//0.1 // derivative gain
+	double pidXout; // pitch output from PID
+
+	// Defining PID Controller Function as pitchPID
+		AutoPID pitchPID(&pitch,&setpoint,&pidXout,Output_min,Output_max, Kp, Ki, Kd);
+
 
 void setup()
 {
 	// SD card intialization
-	// Open serial communications and wait for port to open:
-	Serial.begin(9600);
-	while (!Serial)
-	{
-		; // wait for serial port to connect. Needed for native USB port only
-	}
+		// Open serial communications and wait for port to open:
+		Serial.begin(9600);
+		while (!Serial)
+		{
+			; // wait for serial port to connect. Needed for native USB port only
+		}
 
-	Serial.print("Initializing SD card...");
+		Serial.print("Initializing SD card...");
 
-	if (!SD.begin(10))
-	{
-		Serial.println("initialization failed!");
-		while (1);
-	}
-	Serial.println("initialization done.");
+		if (!SD.begin(10))
+		{
+			Serial.println("initialization failed!");
+			while (1);
+		}
+		Serial.println("initialization done.");
 
 	// Intialization of MPU6050
-	Wire.begin();
-	Serial.begin(9600);
-	Serial.println("Initialize MPU");
-	byte status = mpu.begin();
-	Serial.println(status);
-	while(status!=0)
-	{ } // stop everything if could not connect to MPU6050
-	Serial.println(F("Calculating offsets, do not move MPU6050"));
-	// mpu.upsideDownMounting = true; //uncomment this line if the MPU6050 is mounted upside-down
-	mpu.calcOffsets(); // gyro and accelero
-	Serial.println("Done!\n");
+		Wire.begin();
+		Serial.begin(9600);
+		Serial.println("Initialize MPU");
+		byte status = mpu.begin();
+		Serial.println(status);
+		while(status!=0)
+		{ } // stop everything if could not connect to MPU6050
+		Serial.println(F("Calculating offsets, do not move MPU6050"));
+		// mpu.upsideDownMounting = true; //uncomment this line if the MPU6050 is mounted upside-down
+		mpu.calcOffsets(); //To calculate gyro and accelerometer offset
+		Serial.println("Done!\n");
 
-	SD.begin(10); //Initialize the SD card reader
+	//Initialize the SD card reader
+		SD.begin(10); 
 
-	Xservo.attach(servopin_X);
-	Xservo.write(0);
-	Serial.println("Servo Angle =0"); // To display on screen
-
-	delay(2000);
-
-	//Servo rotation as flag signal for igniting
-	Xservo.write(125);
-	Serial.println("Servo Angle =125"); // To display on screen
-	delay(10000); //10s delay
-	Xservo.write(85);// Flag Servo signal at 90 deg (physical) =85 deg(electrical)
-	Serial.println("Servo Angle =85"); // To display on screen
-	delay(2000);
+	//Initialize the servo at pin D3
+		Xservo.attach(servopin_X);
+		Xservo.write(0);
+		Serial.println("Servo Angle =0"); // To display on screen
+		delay(2000);
+	
+	//Flag signal to alert when to ignite 
+		//Servo rotation as flag signal for igniting
+		Xservo.write(125);
+		Serial.println("Servo Angle =125"); // To display on screen
+		delay(10000); //10s delay
+		Xservo.write(servoAngleDefault);// Flag Servo signal at 90 deg (physical)
+		Serial.println("Servo Angle = ", servoAngleDefault); // To display on screen
+		delay(2000);
 	pitchPID.setTimeStep(40);//set PID update interval to 40ms
 
 }
 
 void loop()
 {
-	Serial.println("Loop started"); // To display on screen
+	Serial.println("Loop started"); // To display on screen that the loop has started
+	
 	mpu.update();
 
-	myTime = millis();
-
-	angleFile = SD.open("pitchAng.txt",FILE_WRITE); // File saved as SAMPLE.txt
+	// Measurement File saved in SD Card as pitchAng.txt
+	
+	myTime = millis(); // To store the sammpling time
+	angleFile = SD.open("pitchAng.txt",FILE_WRITE); 
 	if (angleFile)
 	{
 		pitch= mpu.getAngleY();
@@ -103,16 +124,23 @@ void loop()
 		angleFile.print(",");
 		angleFile.println(pitch); //write angle data to card 
 		angleFile.close();
-		Serial.println("pitch");
+		
+		Serial.println("pitch"); // To display pitch angle on screen
 		Serial.println(pitch);
 		delay(100);
 	}
 
-	pitchPID.run();
+	
+	//PID Controller
+		pitchPID.run();
+		pidXout=servoAngleDefault+pidXout;//servoAngleDefault (i.e. offset of servo = 85 deg)
+		Xservo.write(pidXout); //Giving the command angle to the servo
+		
+		//To diaplay PID output on screen
+		Serial.println("pidXout");
+		Serial.println(pidXout);
 
-	pidXout=servoAngleDefault+pidXout;//offset of pitch servo 85
-	Serial.println("pidXout");
-	Serial.println(pidXout);
-	Xservo.write(pidXout);
-	delay(100);
+		delay(100);
+	
+	
 }
